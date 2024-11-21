33c075ade82121c8ea7bce2a6bfc118d00d35fba
Vaibhav Tanwar <93938070+Vaibtan@users.noreply.github.com>
2024-11-21 22:56:59 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,6 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
-engine = create_engine(os.environ["DATABASE_URL"], echo = True)
+DATABASE_URL = 'postgresql+psycopg2://postgres:88x7kt3b88@localhost:5432/Wildlife-Monitoring'
+engine = create_engine(DATABASE_URL, echo = True)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
