a8424ebe18c935a0f3afdfa6ce68987763587ad1
am3-y <ameya21447@iiitd.ac.in>
2024-11-24 13:35:13 +0530

    added rawimageapi

---
+++ frontend/src/api/rawImageApi.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const fetchRawImages = async () => {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/raw-images');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching raw images:', error);
+    throw error;
+  }
+};
