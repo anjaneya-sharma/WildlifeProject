e9593edd3d4d2880f5b1742ce570cb3587bdd41c
Vaibhav Tanwar <93938070+Vaibtan@users.noreply.github.com>
2024-12-10 13:05:08 +0530

    Update imageApi.js

--- frontend/src/api/imageApi.js
+++ frontend/src/api/imageApi.js
@@ -2,11 +2,31 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://127.0.0.1:8000';
 
-export const fetchImages = () => {
-  return axios.get(`${API_BASE_URL}/images`)
-    .then(response => response.data.images)
-    .catch(error => {
-      console.error('Error fetching images:', error);
-      throw error;
-    });
+export const fetchImages = async (className = 'All') => {
+  const url = `${API_BASE_URL}/detection-images/${encodeURIComponent(className)}`;
+
+  console.log(`Fetching images from URL: ${url}`);
+
+  try {
+    const response = await axios.get(url);
+    console.log('Fetched images:', response.data);
+    const images = response.data.map(imageData => ({
+      ...imageData,
+      src: `data:image/jpeg;base64,${imageData.data}`
+    }));
+
+    return images;
+  } catch (error) {
+    if (error.response) {
+      // not 2xx
+      console.error('Error fetching images:', error.response.status, error.response.data);
+    } else if (error.request) {
+      // no response received
+      console.error('Error fetching images: No response received', error.request);
+    } else {
+      // hopefully never
+      console.error('Error fetching images:', error.message);
+    }
+    throw error;
+  }
 };
